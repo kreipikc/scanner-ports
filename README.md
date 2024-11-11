7335bbbbc23c7a344e8ee80a0c44e0174055f308
kreipikc <basmenov5kir@gmail.com>
2024-11-12 02:44:31 +0300

    Added the ability to scan addresses from a file

--- README.md
+++ README.md
@@ -18,8 +18,16 @@ You can also configure settings such as:
 - `last_port` - specify the port to which the scan will take place, _default: **1024**_ (accpet **int**),
 - `max_buffer` - here, specify the number for the goroutine buffer, <u>change it if you understand why</u>, _default: **100**_ (accept **int**).
 - `save_format` - if you want to save the result, then add it in what format, _default: **none**_ (accepts **json**, **txt**)
+- `document` - if you need to scan several addresses from a txt file (addresses should be in the column line by line), then specify the path to it
 
-Launch example:
+### **!!!Use only address or only document!!!**
+
+Launch example for 1 address:
 ```bash
 go run main.go --address scanme.nmap.org --first_port 1 --last_port 1024 --max_buffer 100 --save_format json
+```
+
+Launch example for document with address:
+```bash
+go run main.go --document file.txt --first_port 1 --last_port 1024 --max_buffer 100 --save_format json
 ```
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -16,12 +17,13 @@ type Ports struct {
 	Port int `json:"port"`
 }
 
-func InitFlags() (string, int, int, int, string) {
-	addressFlag := flag.String("address", "scanme.nmap.org", "Address for scanning ports")
+func InitFlags() (string, int, int, int, string, string) {
+	addressFlag := flag.String("address", "", "Address for scanning ports")
 	firstPortFlag := flag.String("first_port", "1", "Scanning will start from this port")
 	lastPortFlag := flag.String("last_port", "1024", "There will be scanning before this port")
 	bufferChanFlag := flag.String("max_buffer", "100", "This number is responsible for the number of port scans at the moment, it makes no sense to put it above the last port")
 	saveFormatFlag := flag.String("save_format", "", "Accept: json, txt")
+	documentFlag := flag.String("document", "", "Path for document with addresses for scanning")
 
 	flag.Parse()
 
@@ -50,10 +52,16 @@ func InitFlags() (string, int, int, int, string) {
 		os.Exit(1)
 	}
 
-	return address, firstPort, lastPort, bufferChan, saveFormat
+	document := *documentFlag
+	if _, err := os.Stat(document); os.IsNotExist(err) && document != "" {
+		fmt.Println("No document found")
+		os.Exit(1)
+	}
+
+	return address, firstPort, lastPort, bufferChan, saveFormat, document
 }
 
-func SaveResult(result []int, format string) error {
+func SaveResult(result []int, format string, address string) error {
 	if _, err := os.Stat("result"); os.IsNotExist(err) {
 		err := os.Mkdir("result", 0755)
 		if err != nil {
@@ -63,7 +71,7 @@ func SaveResult(result []int, format string) error {
 	}
 
 	if format == "txt" {
-		file, err := os.Create("result/result.txt")
+		file, err := os.Create(fmt.Sprintf("result/OpenPorts_%s.txt", address))
 		if err != nil {
 			fmt.Println("Unable to create TXT file:", err)
 			return err
@@ -79,7 +87,7 @@ func SaveResult(result []int, format string) error {
 			}
 		}
 	} else if format == "json" {
-		file, err := os.Create("result/result.json")
+		file, err := os.Create(fmt.Sprintf("result/OpenPorts_%s.json", address))
 		if err != nil {
 			fmt.Println("Unable to create JSON file:", err)
 			return err
@@ -101,9 +109,22 @@ func SaveResult(result []int, format string) error {
 	return nil
 }
 
+func ReadAddressDocument(doc string) []string {
+	file, _ := os.Open(doc)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	result := []string{}
+
+	for scanner.Scan() {
+		result = append(result, strings.TrimSpace(scanner.Text()))
+	}
+	return result
+}
+
 func worker(ports chan int, result chan int, adrs string) {
 	for port := range ports {
-		fmt.Printf("Test request for %d\n", port)
 		address := fmt.Sprintf("%s:%d", adrs, port)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
@@ -115,63 +136,86 @@ func worker(ports chan int, result chan int, adrs string) {
 	}
 }
 
+func starter(address string, firstPort int, lastPort int, bufferChan int, saveFormat string) {
+	ports := make(chan int, bufferChan)
+	results := make(chan int)
+	var openPorts []int
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, results, address)
+	}
+
+	fmt.Printf("\nScanning %s...", address)
+	startTime := time.Now()
+
+	go func() {
+		for numPort := firstPort; numPort <= lastPort; numPort++ {
+			ports <- numPort
+		}
+	}()
+
+	for i := firstPort; i <= lastPort; i++ {
+		port := <-results
+		if port != 0 {
+			openPorts = append(openPorts, port)
+		}
+	}
+
+	close(ports)
+	close(results)
+
+	elapsedTime := time.Since(startTime)
+	sort.Ints(openPorts)
+
+	fmt.Println("\nResults for " + address)
+	fmt.Println("Scanning time:", elapsedTime)
+	if len(openPorts) != 0 {
+		fmt.Printf("Found: %d element\nOpen ports:\n", len(openPorts))
+		for _, port := range openPorts {
+			fmt.Printf("%d open\n", port)
+		}
+
+		if saveFormat != "" {
+			err := SaveResult(openPorts, saveFormat, address)
+			if err != nil {
+				os.Exit(1)
+			}
+		}
+	} else {
+		fmt.Println("No port is open")
+	}
+}
+
 func main() {
-	address, firstPort, lastPort, bufferChan, saveFormat := InitFlags()
+	address, firstPort, lastPort, bufferChan, saveFormat, document := InitFlags()
 
 	if firstPort > lastPort {
 		fmt.Println("Error: The first port cannot exceed the last port.")
 	} else if firstPort <= 0 || lastPort <= 0 {
 		fmt.Println("Error: The port cannot be <= 0.")
 	} else {
-		fmt.Println("Press 'Enter' to start scan...")
-		fmt.Scanf(" ")
+		if address != "" && document == "" {
+			fmt.Println("Press 'Enter' to start scan...")
+			fmt.Scan(" ")
 
-		ports := make(chan int, bufferChan)
-		results := make(chan int)
-		var openPorts []int
+			starter(address, firstPort, lastPort, bufferChan, saveFormat)
+		} else if address == "" && document != "" {
+			fmt.Println("Press 'Enter' to start scan...")
+			fmt.Scan(" ")
 
-		for i := 0; i < cap(ports); i++ {
-			go worker(ports, results, address)
-		}
+			addressList := ReadAddressDocument(document)
 
-		fmt.Println("Scanning...")
-		startTime := time.Now()
-
-		go func() {
-			for numPort := firstPort; numPort <= lastPort; numPort++ {
-				ports <- numPort
-			}
-		}()
-
-		for i := firstPort; i <= lastPort; i++ {
-			port := <-results
-			if port != 0 {
-				openPorts = append(openPorts, port)
-			}
-		}
-
-		close(ports)
-		close(results)
-
-		elapsedTime := time.Since(startTime)
-		sort.Ints(openPorts)
-
-		fmt.Println("\nScanning time:", elapsedTime)
-		if len(openPorts) != 0 {
-			fmt.Printf("Found: %d element\nOpen ports:\n", len(openPorts))
-			for _, port := range openPorts {
-				fmt.Printf("%d open\n", port)
-			}
-
-			if saveFormat != "" {
-				err := SaveResult(openPorts, saveFormat)
-				if err != nil {
-					os.Exit(1)
+			if len(addressList) != 0 {
+				for _, adrs := range addressList {
+					starter(adrs, firstPort, lastPort, bufferChan, saveFormat)
 				}
+			} else {
+				fmt.Println("File empty")
+				os.Exit(1)
 			}
 		} else {
-			fmt.Println("No port is open")
+			fmt.Println("Use only address or document")
+			os.Exit(1)
 		}
-		fmt.Scanf(" ")
 	}
 }
